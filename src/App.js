24ef369ef71ad8470119394e6a1d649377a084e3
Rasheeda <rassalaam@gmail.com>
2022-02-08 03:00:18 +0100

    beautified, a tad

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import "./App.css";
+import Popup from "./Popup";
 import "regenerator-runtime/runtime.js";
 import submitForCompletion from "../functions/completion";
 
 const App = () => {
   const [output, setOutput] = useState(null);
   const [inputText, setInputText] = useState("");
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -12,20 +15,38 @@ const App = () => {
     setInputText("");
   };
 
+  const togglePopup = () => {
+    setIsPopupOpen(!isPopupOpen);
+  };
+
   return (
-    <div>
-      <h4>Type your text below, so that the robot can repeat you!</h4>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          type="text"
-          value={inputText}
-          placeholder='Type here, beginning with "Say..."'
-          onChange={(event) => setInputText(event.target.value)}
+    <div className="instruction-input-submit">
+      <h3>
+        Type your text below, so that we can mimic you!
+      </h3>
+      <textarea
+      className="input"
+        type="text"
+        value={inputText}
+        placeholder='Type here, beginning with "Say..."'
+        onChange={(event) => setInputText(event.target.value)}
+      />
+      <br />
+      <button
+      className="submit-button"
+        onClick={async () => {
+          await handleSubmit(event);
+          await togglePopup();
+        }}
+      >
+        Submit
+      </button>
+      {isPopupOpen && (
+        <Popup
+          content={<>{output ? <p>{output}</p> : null}</>}
+          handleClose={togglePopup}
         />
-        <br />
-        <button>Submit</button>
-        <div>{output ? <p>{output}</p> : null}</div>
-      </form>
+      )}
     </div>
   );
 };
